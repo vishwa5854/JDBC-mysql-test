9c09032943cb98b695fabdb7658717d46d77850b
Vishwanath Bondugula <vishwanath5854@outlook.com>
2020-08-12 20:41:00 +0530

    added insert and retrieve

--- src/main/java/MySQLConnection.java
+++ src/main/java/MySQLConnection.java
@@ -3,12 +3,26 @@ import java.sql.*;
 public class MySQLConnection {
     public static void main(String[] args) throws SQLException {
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/crueger", "root", "password");
+        String name = "Srinivas";
+        String query = "INSERT INTO lol (name) values ( ? )";
+        insert(query, name, connection);
+        retrieve("select *from lol", connection);
+        connection.close();
+    }
+
+
+    private static void insert(String query, String params, Connection connection) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, params);
+        preparedStatement.execute();
+    }
+
+    private static void retrieve(String query, Connection connection) throws SQLException {
         Statement statement = connection.createStatement();
-        String query = "select * from lol";
         ResultSet resultSet = statement.executeQuery(query);
         while (resultSet.next()){
             System.out.println(resultSet.getString(1));
         }
-        connection.close();
     }
+
 }
